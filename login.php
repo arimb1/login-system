26b6ac94fba15ae0b4d43f454351b82b7e5674d9
arimb1 <95376692+arimb1@users.noreply.github.com>
2022-01-14 11:30:49 +0800

    pas udh login, muncul "Logged in!"

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-
+include "db_conn.php";
 if (isset($_POST['uname']) && isset($_POST['pass'])) {
     
     function validate($data){
@@ -13,20 +13,32 @@ if (isset($_POST['uname']) && isset($_POST['pass'])) {
     $pass = validate($_POST['pass']);
 
     if (empty($uname)) {
-        header("Location : index.php?error=Username is required");
+        header("Location: index.php?error=Username is required");
         exit();
 
-    }else if(empty($pass)){
-        header("Location : index.php?error=Password is required");
+    }else if(empty($pass)) {
+        header("Location: index.php?error=Password is required");
         exit();
 
     }else{
-        echo "Valid Input";
-    }
-    
-    }
+        $sql = "SELECT * FROM users WHERE username='$uname' AND password='$pass'";
+        
+        $result = mysqli_query($conn, $sql);
 
-else{
-    header("Lokcation: index.php");
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $uname && $row['password'] === $pass) {
+                echo "Logged in!";
+            }else{
+                    header("Location: index.php?error=Incorrect Username or Password");
+                    exit();
+            }
+        }else{
+            header("Location: index.php?error=Incorrect Username or Password");
+            exit();
+        }
+}
+}else{
+    header("Location: index.php");
     exit();
 }
